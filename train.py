b1f7a476103980db66ee6b52e6354d4ac6e05e59
Sangryu Park <psrpsj@gmail.com>
2022-12-20 08:43:56 +0000

    finish non kfold train

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from argument import TrainingArguments, TrainModelArguments
 from dataset import CustomDataset
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import StratifiedKFold, train_test_split
-from trainer import CustomLossTrainer, CustomTrainer
+from trainer import CustomTrainer
 from transformers import (
     AutoConfig,
     AutoModelForSequenceClassification,
@@ -15,7 +15,7 @@ from transformers import (
     HfArgumentParser,
     set_seed,
 )
-from utils import label_to_num, stopword
+from utils import label_to_num
 
 
 def compute_metrics(pred):
@@ -34,3 +34,54 @@ def train():
 
     print(f"Current model is {model_args.model_name}")
     print(f"Current device is {device}")
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model_args.model_name
+    )
+    set_seed(train_args.seed)
+
+    target = ["유형", "극성", "시제", "확실성"]
+    target_num = [4, 3, 3, 2]
+
+    print("### START TRAINING with Non-KFold ###")
+    for idx, t in enumerate(target):
+        print(f"Start Training {t}")
+        data[t] = label_to_num(data[t], t)
+        model_config = AutoConfig.from_preptrained(
+            pretrained_model_name_or_path=model_args.model_name
+        )
+        model_config.num_labels = target_num[idx]
+        model = AutoModelForSequenceClassification(
+            pretrained_model_name_or_path=model_args.model_name, config=model_config
+        )
+        model.to(device)
+        model.train()
+
+        wandb.init(
+            entity="psrpsj",
+            project="sentence",
+            name=model_args.project_name + "_" + t,
+            tags=[model_args.model_name],
+        )
+        wandb.config.update(train_args)
+
+        train_dataset, valid_dataset = train_test_split(
+            data, test_size=0.2, stratify=data[t], random_state=42
+        )
+
+        train = CustomDataset(train_dataset, tokenizer)
+        valid = CustomDataset(valid_dataset, tokenizer)
+
+        trainer = CustomTrainer(
+            model=model,
+            args=train_args,
+            loss_name=model_args.loss_name,
+            train_data=train,
+            eval_data=valid,
+        )
+        model.save_pretrained(
+            os.path.join(train_args.output_dir, model_args.project_name, str(idx))
+        )
+        wandb.finish()
+        print(f"Training {t} finish!")
+    print("### TRAINING FINISH ###")
